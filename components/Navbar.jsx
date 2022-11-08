429ca798d48208a4310efafb0156ad647f9d6700
hardcoregor <spraytown2014@gmail.com>
2022-11-08 19:46:07 +0200

    navbar done, footer in progress

---
+++ components/Navbar.jsx
@@ -0,0 +1,169 @@
+import React, { useState, useEffect, useContext } from 'react';
+
+import { useRouter } from 'next/router';
+import { useTheme } from 'next-themes';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import images from '../assets';
+import Button from './Button';
+
+const MenuItems = ({ isMobile, active, setActive }) => {
+  const generateLink = (i) => {
+    switch (i) {
+      case 0: return '/';
+      case 1: return '/created-nfts';
+      case 2: return '/my-nfts';
+      default:
+        break;
+    }
+  };
+
+  return (
+    <ul className={`list-none flexCenter flex-row ${isMobile && 'flex-col h-full'}`}>
+      {['Explore NFTs', 'Listen NFTs', 'My NFTs'].map((item, i) => (
+        <li
+          key={i}
+          onClick={() => {
+            setActive(item);
+          }}
+          className={isMobile ? `flex flex-row items-center font-poppins font-semibold mx-3 text-4xl mb-8 ${active === item
+            ? 'dark:text-white text-nft-black-1'
+            : 'dark:text-nft-gray-3 text-nft-gray-2 '}
+
+          ` : `flex flex-row items-center font-poppins font-semibold text-base dark:hover:text-white hover:text-nft-dark mx-3 ${active === item
+            ? 'dark:text-white text-nft-black-1'
+            : 'dark:text-nft-gray-3 text-nft-gray-2 '}
+          `}
+        >
+          <Link href={generateLink(i)}>{item}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const ButtonGroup = ({ setActive, router, isMobile }) => {
+  const hasConnected = true;
+
+  return hasConnected ? (
+    <Button
+      btnName="Create"
+      classStyles={isMobile ? 'mx-2 rounded-xl text-2xl' : 'mx-2 rounded-xl'}
+      handleClick={() => {
+        setActive('');
+        router.push('/create-nft');
+      }}
+    />
+  ) : (
+    <Button
+      btnName="Connect"
+      classStyles={isMobile ? 'mx-2 rounded-xl text-2xl' : 'mx-2 rounded-xl'}
+      handleClick={() => {
+        setActive('');
+      }}
+    />
+  );
+};
+
+const Navbar = () => {
+  const { theme, setTheme } = useTheme();
+  const router = useRouter();
+  const [active, setActive] = useState('Explore NFTs');
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <nav className="flexBetween w-full fixed z-10 p-4 flex-row border-b
+    dark:bg-nft-dark
+    bg-white
+    dark:border-nft-black-1
+    border-nft-gray-1"
+    >
+      <div className="flex flex-1 flex-row justify-start">
+        <Link href="/">
+          <div className="flexCenter md:hidden cursor-pointer" onClick={() => { }}>
+            <Image
+              src={images.logo02}
+              objectFit="contain"
+              width={32}
+              height={32}
+              alt="logo"
+            />
+            <p className="dark:text-white text-nft-black-1 font-semibold text-lg ml-1">Flipster</p>
+          </div>
+        </Link>
+        <Link href="/">
+          <div className="hidden md:flex" onClick={() => { }}>
+            <Image
+              src={images.logo02}
+              objectFit="contain"
+              width={32}
+              height={32}
+              alt="logo"
+            />
+          </div>
+        </Link>
+      </div>
+      <div className="flex flex-initial flex-row justify-end">
+        <div className="flex items-center mr-2">
+          <input
+            type="checkbox"
+            className="checkbox"
+            id="checkbox"
+            onChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+          />
+          <label htmlFor="checkbox" className="flexBetween w-8 h-4 dark:bg-white bg-black rounded-2xl p-1 relative label cursor-pointer">
+            <i className="fas fa-sun" style={{ color: 'white' }} />
+            <i className="fas fa-moon" style={{ color: 'black' }} />
+            <div className="w-3 h-3 absolute dark:bg-black bg-white rounded-full ball" />
+          </label>
+        </div>
+
+        <div className="md:hidden flex">
+          <MenuItems active={active} setActive={setActive} />
+          <div className="ml-4">
+            <ButtonGroup setActive={setActive} router={router} />
+          </div>
+        </div>
+      </div>
+
+      <div className="hidden md:flex ml-2">
+        {isOpen
+          ? (
+            <Image
+              src={images.cross}
+              onjectFit="contain"
+              width={20}
+              height={20}
+              alt="close"
+              onClick={() => setIsOpen(false)}
+              className={theme === 'light' && 'filter invert'}
+            />
+          )
+          : (
+            <Image
+              src={images.menu}
+              onjectFit="contain"
+              width={25}
+              height={25}
+              alt="menu"
+              onClick={() => setIsOpen(true)}
+              className={theme === 'light' && 'filter invert'} // Change color of burger menu
+            />
+          )}
+        {isOpen && (
+          <div className="fixed inset-0 top-65 dark:bg-nft-dark bg-white z-10 nav-h flex justify-between flex-col">
+            <div className="flex-1 p-4 ">
+              <MenuItems active={active} setActive={setActive} isMobile />
+            </div>
+            <div className="p-4 border-t dark:border-nft-black-1 border-nft-gray-1 flex justify-center">
+              <ButtonGroup setActive={setActive} router={router} isMobile />
+            </div>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
