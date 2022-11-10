c88070392aa762df72ea346a9f116feed8accf1e
hardcoregor <spraytown2014@gmail.com>
2022-11-10 18:48:12 +0200

    front-end done, smart contract done

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import { useTheme } from 'next-themes';
 
+import Link from 'next/link';
 import images from '../assets';
 // eslint-disable-next-line import/no-cycle
 import { Button } from '.';
@@ -21,16 +22,19 @@ const Footer = () => {
     <footer className="flexCenter flex-col border-t dark:border-nft-black-1 border-nft-gray-1 sm:py-8 py-16">
       <div className="w-full minmd:w-4/5 flex flex-row md:flex-col sm:px-4 px-16">
         <div className="flexStart flex-1 flex-col">
-          <div className="flexCenter cursor-pointer">
-            <Image
-              src={images.logo02}
-              objectFit="contain"
-              width={32}
-              height={32}
-              alt="logo"
-            />
-            <p className="dark:text-white text-nft-black-1 font-semibold text-lg ml-1">Flipster</p>
-          </div>
+          <Link href="/">
+            <div className="flexCenter cursor-pointer">
+
+              <Image
+                src={images.logo02}
+                objectFit="contain"
+                width={32}
+                height={32}
+                alt="logo"
+              />
+              <p className="dark:text-white text-nft-black-1 font-semibold text-lg ml-1">Flipster</p>
+            </div>
+          </Link>
           <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-base mt-6">Get the lastes Updates</p>
           <div className="flexBetween md:w-full minlg:w-557 w-357 mt-6 dark:bg-nft-black-2 bg-white border dark:border-nft-black-2 border-nft-gray-2 rounded-md">
             <input type="email" placeholder="Your Email" className="h-full flex-1 w-full dark:bg-nft-black-2 bg-white px-4 rounded-md dark:text-white text-nft-black-1 font-normal text-xs minlg:text-lg outline-none" />
